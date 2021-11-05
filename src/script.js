be393ef5db92b808d1068fc8fbf6f111d5d979e5
user109436 <zedekfelix7@gmail.com>
2021-11-05 19:05:56 +0800

    SJF Non-Preemptive with Gui Implementation

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 class Process {
   constructor(name, arrival, burst) {
     this.name = name;
-    this.arrival = arrival;
     this.burst = burst;
+    this.arrival = arrival;
     this.completionTime = 0;
     this.turnAroundTime = 0;
     this.waitingTime = 0;
@@ -15,8 +15,11 @@ class Schedule {
     this.numProcess = numProcess;
     this.process = [];
     this.timeElapse = 0;
-    this.averageTurnAroundTime = 0;
-    this.averageWaitingTime = 0;
+    this.solutions = {
+      averageWaitingTime: [],
+      averageTurnAroundTime: [],
+      numProcess: this.numProcess,
+    };
     this.error;
 
     for (let i = 0; i < numProcess; i++) {
@@ -136,35 +139,119 @@ class Schedule {
       if (timeElapse) {
         let que = this.timeElapseGreaterThanArrival(timeElapse, sortedQue);
         que = this.sortBurst(que); //find lowest burst in que
+        this.printQue(que);
         let q = que[0];
         //find the right processName then assign the values
         p = this.getProcess(q.name);
         p.completionTime = timeElapse += q.burst; //execute lowest burst
+        this.printActivity(p);
         p.turnAroundTime = timeElapse - q.arrival;
         p.waitingTime = p.turnAroundTime - q.burst;
         p.responseTime = p.waitingTime;
-        this.averageWaitingTime += p.waitingTime;
-        this.averageTurnAroundTime += p.turnAroundTime;
         this.removeInSortedQue(q.name, sortedQue); //find process name in sorted que then remove
         que.splice(0, 1); //remove from que
       } else {
         p.completionTime = timeElapse += p.burst;
+        this.printActivity(p);
         p.turnAroundTime = timeElapse - p.arrival;
         p.waitingTime = p.turnAroundTime - p.burst;
         p.responseTime = p.waitingTime;
-        this.averageWaitingTime += p.waitingTime;
-        this.averageTurnAroundTime += p.turnAroundTime;
         sortedQue.splice(0, 1);
       }
     }
-    this.averageTurnAroundTime /= this.numProcess;
-    this.averageWaitingTime /= this.numProcess;
     this.process = sortedArrival;
+    this.timeElapse = timeElapse;
     return sortedArrival;
+  };
+  printQue = (que) => {
+    let queTxt = "";
+    for (let i in que) {
+      queTxt += `${que[i].name} `;
+    }
+    new Log(`Que: ${queTxt}`).logHeadingElement();
+  };
+  printActivity = (p) => {
+    new Log(p.name + " is done", `@ ${p.completionTime}ms`).logActvityElement();
+  };
+  solution = () => {
+    let s = this.solutions;
+    let sumWT = 0,
+      sumTAT = 0;
+    for (let i = 0; i < this.numProcess; i++) {
+      let p = this.process[i];
+      s.averageWaitingTime.push(
+        `${p.name} = (${p.turnAroundTime} - ${p.burst}) = ${p.waitingTime}`
+      );
+      s.averageTurnAroundTime.push(
+        `${p.name} = (${p.completionTime} - ${p.arrival}) = ${p.turnAroundTime}`
+      );
+      sumTAT += p.turnAroundTime;
+      sumWT += p.waitingTime;
+    }
+    s.averageWaitingTime.push(
+      `Average Waiting Time:(${sumWT} / ${this.numProcess}) = ${
+        sumWT / this.numProcess
+      }ms`
+    );
+    s.averageTurnAroundTime.push(
+      `Average Turn Around Time:(${sumTAT} / ${this.numProcess}) = ${
+        sumTAT / this.numProcess
+      }ms`
+    );
+    return this.solutions;
   };
 }
 
-class GanttChartElement {}
+class GanttChartElement {
+  constructor(Schedule) {
+    this.schedule = Schedule;
+    this.process = this.schedule.sortCompletionTime();
+    this.numProcess = this.process.length;
+    this.timeElapse = this.schedule.timeElapse;
+    this.taskElement();
+  }
+  taskElement = () => {
+    let x = this.process;
+    let ganttChartContent = document.getElementsByClassName(
+      "gantt-chart-content"
+    )[0];
+    for (let i = 0; i < x.length; i++) {
+      let p = x[i];
+      let width = this.calculateWidthPercentage(p);
+      let el = this.itemTask(p, width, i);
+      ganttChartContent.innerHTML += el;
+    }
+  };
+  calculateWidthPercentage = (process) => {
+    return (process.completionTime * 100) / this.timeElapse;
+  };
+
+  itemTask = (process, width, i) => {
+    let p = process;
+    let el = `
+       <div
+              style="width: ${width}%"
+              class="
+                process
+                ${i % 2 == 0 ? "bg-blue-300" : "bg-green-300"}
+                border-2 border-green-800  ${i ? "border-r-1  border-l-0" : ""}
+                relative
+              "
+            >
+              <p class="process-name p-2">${p.name}</p>
+      `;
+    if (i == 0) {
+      el += `
+              <span class="absolute left-0 mt-3">${p.arrival}</span>
+              <span class="absolute right-0 mt-3">${p.completionTime}</span>
+      </div>
+      `;
+    }
+    el += ` <span class="absolute right-0 mt-3">${p.completionTime}</span>
+      </div>`;
+    return el;
+  };
+}
 
 class Log {
   constructor(name = "", activity = "", icon = "fas fa-microchip") {
@@ -182,12 +269,12 @@ class Log {
                   p-2
                 "
               >
-                <div class="self-center p-2 ${textColor}">
-                  <i class="${this.icon} fa-1x"></i> ${this.name} ${this.activity}
+                <div class="self-center w-full flex justify-between p-2 ${textColor}">
+                  <p><i class="${this.icon} fa-1x"></i> ${this.name} </p> <span class="text-gray-500 text-right">${this.activity}</span>
                 </div>
         </div>
         `;
-    this.appendLog(el);
+    this.append(el);
     return 1;
   };
   logHeadingElement = () => {
@@ -206,14 +293,12 @@ class Log {
                 ${this.name} ${this.activity}
               </div>
         `;
-    this.appendLog(el);
+    this.append(el);
     return 1;
   };
-  appendLog = (el) => {
-    let logContent = document.querySelectorAll(".log-content")[0];
-    let log = document.createRange().createContextualFragment(el);
-    logContent.insertBefore(log, logContent.lastElementChild.nextSibling);
-    return 1;
+  append = (childElement, className = "log-content") => {
+    let parent = document.getElementsByClassName(`${className}`)[0];
+    parent.innerHTML += childElement;
   };
 }
 
@@ -235,23 +320,143 @@ class TableData {
     tbody.appendChild(tr);
   };
 }
-const x = {
-  process: 4,
-  arrival: [0, 0, 0, 0],
-  burst: [6, 8, 7, 3],
-};
+
+class SolutionElement {
+  constructor(solution) {
+    this.solution = solution;
+
+    this.computationElement();
+  }
+  computationElement = () => {
+    let x = this.solution;
+    for (let i = 0; i <= x.numProcess; i++) {
+      this.append("average-waiting-time", `<p>${x.averageWaitingTime[i]}</p>`);
+      this.append(
+        "average-turn-around-time",
+        `<p>${x.averageTurnAroundTime[i]}</p>`
+      );
+    }
+  };
+
+  append = (className, childElement) => {
+    let parent = document.getElementsByClassName(`${className}`)[0];
+    parent.innerHTML += childElement;
+  };
+}
 
 function isObject(obj) {
   return obj && typeof obj === "object" && obj.constructor === Object;
 }
 
-const y = new Schedule(x.process, x.arrival, x.burst);
-y.execute();
-let z = y.sortProcessName();
-if (isObject(z)) {
-  console.log(z);
-} else {
-  for (let i = 0; i < z.length; i++) {
-    new TableData(z[i]);
+let startBtn = document.getElementsByClassName("start-btn")[0];
+
+startBtn.addEventListener("click", () => {
+  let logContent = document.getElementsByClassName("log-content")[0];
+  let tableData = document.getElementsByClassName("table-data")[0];
+  let AWT = document.getElementsByClassName("average-waiting-time")[0];
+  let ATAT = document.getElementsByClassName("average-turn-around-time")[0];
+  let ganttChart = document.getElementsByClassName("gantt-chart-content")[0];
+  logContent.innerHTML = "";
+  tableData.innerHTML = "";
+  AWT.innerHTML = "";
+  ATAT.innerHTML = "";
+  ganttChart.innerHTML = "";
+  let numProcess = document.getElementById("num-process");
+  let arrivals = document.getElementById("arrivals");
+  let burst = document.getElementById("burst");
+  let schedule = {};
+  let error = 0;
+
+  // number of process
+  numProcess = parseInt(numProcess.value);
+  if (numProcess) {
+    schedule.process = numProcess;
+  } else {
+    error += 1;
+    new Log("Process:", "Positive Numbers Only").logActvityElement(
+      "text-red-400"
+    );
   }
-}
+
+  // arrivals
+  let inputArrivals = arrivals.value.split(" ");
+  let sanitizedArrivals = validateInputs(
+    inputArrivals,
+    "arrivals:Positive Numbers Only"
+  );
+  if (sanitizedArrivals.error > 0) {
+    error += sanitizedArrivals.error;
+    new Log(
+      sanitizedArrivals.error,
+      sanitizedArrivals.message
+    ).logActvityElement("text-red-400");
+  } else if (sanitizedArrivals.data.length !== numProcess) {
+    error += 1;
+    new Log(
+      "Arrival:",
+      "Not match with the number of Process"
+    ).logActvityElement("text-red-400");
+  } else {
+    schedule.arrivals = sanitizedArrivals.data;
+  }
+  // ensure to fill the corresponding values  process number
+
+  //burst
+  let inputBurst = burst.value.split(" ");
+  let sanitizedBurst = validateInputs(
+    inputBurst,
+    "burst: Greater than 0 & Positive Numbers Only ",
+    1
+  );
+  if (sanitizedBurst.error > 0) {
+    error += sanitizedBurst.error;
+    new Log(sanitizedBurst.error, sanitizedBurst.message).logActvityElement(
+      "text-red-400"
+    );
+  } else if (sanitizedBurst.data.length !== numProcess) {
+    error += 1;
+    new Log("Burst:", "Not match with the number of Process").logActvityElement(
+      "text-red-400"
+    );
+  } else {
+    schedule.burst = sanitizedBurst.data;
+  }
+  // ensure to fill the corresponding values  process number
+
+  if (error == 0) {
+    const task = new Schedule(
+      schedule.process,
+      schedule.arrivals,
+      schedule.burst
+    );
+
+    task.execute();
+    let sortedTask = task.sortProcessName();
+    if (isObject(sortedTask)) {
+      new Log("Error:", sortedTask.error).logActvityElement("text-red-400");
+    } else {
+      for (let i = 0; i < sortedTask.length; i++) {
+        new TableData(sortedTask[i]);
+      }
+    }
+
+    new SolutionElement(task.solution());
+    new GanttChartElement(task);
+  }
+});
+
+validateInputs = (input, msg, min = 0) => {
+  let s = input;
+  let sanitized = { error: 0, msg: "", data: [] };
+
+  for (let i = 0; i < s.length; i++) {
+    s[i] = parseFloat(s[i]);
+    if (s[i] >= min) {
+      sanitized.data.push(s[i]);
+    } else {
+      sanitized.error += 1;
+      sanitized.message = msg;
+    }
+  }
+  return sanitized;
+};
